ddefa34dd467005f8444d1848977ff1f228c06a0
Morgan Wall <mwall1992@gmail.com>
2013-09-28 21:57:26 +1000

    Consolidated the game into a single view controller

---
+++ Harlequin-Havoc/Classes/States/HHApplicationViewController.h
@@ -0,0 +1,13 @@
+/**
+ * @filename HHApplicationViewController.h
+ * @author Morgan Wall
+ * @date 28-9-2013
+ *
+ * @brief The primary view controller responsible for managing the entire game.
+ */
+
+#import <UIKit/UIKit.h>
+
+@interface HHApplicationViewController : UIViewController
+
+@end

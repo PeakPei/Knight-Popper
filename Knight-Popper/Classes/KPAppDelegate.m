75ca166475321c7d75bf87c774c389b9ab04ae9f
Morgan Wall <mwall1992@gmail.com>
2014-02-16 15:56:45 +1000

    Hotfix for launch image not displaying on iOS 7 R4

--- Knight-Popper/Classes/KPAppDelegate.m
+++ Knight-Popper/Classes/KPAppDelegate.m
@@ -35,4 +35,8 @@
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
 }
 
+- (NSUInteger)application:(UIApplication *)application supportedInterfaceOrientationsForWindow:(UIWindow *)window {
+    return UIInterfaceOrientationMaskLandscape;
+}
+
 @end
